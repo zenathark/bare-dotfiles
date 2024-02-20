d3ed1735e4a17f3ec9a0ec4ff3dcbc567f0dedac
Juan Galan Hernandez <majuanh@amazon.com>
2024-02-20 00:33:42 +0000

    Remove neotree to switch to netrw

--- nvim/.config/nvim/lua/zenathark/plugins/telescope.lua
+++ nvim/.config/nvim/lua/zenathark/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 
+      { 'nvim-lua/plenary.nvim' },
+      {'ahmedkhalf/project.nvim'},
+    },
     config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
